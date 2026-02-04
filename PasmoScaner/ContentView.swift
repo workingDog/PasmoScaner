08daa2d75a4123029f3ca9f3be33f9a833db3915
workingdog <ringowathelet@gmail.com>
2026-02-04 10:19:11 +0900

    Initial Commit

---
+++ PasmoScaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PasmoScaner
+//
+//  Created by Ringo Wathelet on 2026/02/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PasmoScaner/PasmoScanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasmoScanerApp.swift
+//  PasmoScaner
+//
+//  Created by Ringo Wathelet on 2026/02/04.
+//
+
+import SwiftUI
+
+@main
+struct PasmoScanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
