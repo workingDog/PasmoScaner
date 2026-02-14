bd62cdc0647b5102fb810a68ece6fc99be09c005
workingdog <ringowathelet@gmail.com>
2026-02-14 15:00:24 +0900

    initial commit

--- PasmoScaner/ReaderSession.swift
+++ PasmoScaner/ReaderSession.swift
@@ -16,7 +16,7 @@ final class ReaderSession: NSObject, NFCTagReaderSessionDelegate {
         try await withCheckedThrowingContinuation { continuation in
             self.continuation = continuation
             session = NFCTagReaderSession(pollingOption: .iso18092, delegate: self)
-            session?.alertMessage = "Tap iPhone to PASMO"
+            session?.alertMessage = "Tap PASMO on Neko"
             session?.begin()
         }
     }
